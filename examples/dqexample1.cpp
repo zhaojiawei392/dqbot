6d194ba760da0e94c4a9fb50fe96300304f3f123
kai <zhaojiawei392@gmail.com>
2024-11-04 11:40:51 +0900

    added json constructed robot example

--- examples/dqexample1.cpp
+++ examples/dqexample1.cpp
@@ -22,10 +22,11 @@
  *	   \date 2023-2024
  */
 
-#include <array>
 #include "dqbot.hpp"
+#include <array>
 
-int main() {
+template<typename Scalar, std::size_t size>
+dqbot::SerialManipulator<Scalar, size> construct_robot_from_args() {
     std::array<std::array<float, 6>, 5> dh {
         std::array<float, 6>{0,       M_PI,      -M_PI_2,       0,      0,   0},
         std::array<float, 6>{0.1735,  0,         0,       -0.1182,  0,      -0.264},
@@ -50,6 +51,22 @@ int main() {
     std::array<float, 6> joint_positions {0,0,0,0,0,0};
 
     dqbot::SerialManipulator<float, 6> robot(dh, limits, joint_positions);
+    return robot;
+}
+
+
+template<typename Scalar, std::size_t size>
+dqbot::SerialManipulator<Scalar, size> construct_robot_from_json() {
+    const std::string json_path("../examples/robot.json");
+    const std::array<float, 6> joint_positions {0,0,0,0,0,0};
+
+    dqbot::SerialManipulator<float, 6> robot(json_path, joint_positions);
+    return robot;
+}
+
+int main() {
+using scalar_t = float;
+    auto robot = construct_robot_from_args<scalar_t, 6>();
 
     dqbot::Posef x_init = robot.end_pose();
     dqbot::Tranf t_init = x_init.translation();
@@ -60,13 +77,13 @@ int main() {
 
     while (true)
     {   
-        dqbot::Tranf offset = t_init + dqbot::Tranf(-0.02, radius * cos(rad_speed*i), radius * sin(rad_speed*i) + 0.05);
+        dqbot::Tranf td = t_init + dqbot::Tranf(-0.02, radius * cos(rad_speed*i), radius * sin(rad_speed*i) + 0.05);
         ++i;
-        dqbot::Posef xd = dqbot::Posef::build_from(r_init, offset);
+        dqbot::Posef xd = dqbot::Posef::build_from(r_init, td);
 
         robot.update(xd);
 
-        // std::cout << robot.end_pose().rotation() << "\n" << xd.rotation() << "\n";
+        std::cout << (robot.end_pose().translation() - td).norm() << "\n";
     }
 
 }             
